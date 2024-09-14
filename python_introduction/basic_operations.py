100cbc8d02953f6f606741f591ca90011269e152
Balungile <danca0717342125@gmail.com>
2024-09-14 21:05:33 +0200

    added basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+subtration = number1 - number2
+multiplication = number1 * number2
+print(f"addition of {number1} and {number2} is {addition} ")
+print(f"subtration of {number1} and {number2} is {subtration} ")
+print(f"multiplication of {number1} and {number2} is {multiplication} ")
+
