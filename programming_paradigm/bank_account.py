778125a6c582e87b777b739a43765a1787e04a49
Balungile <danca0717342125@gmail.com>
2024-10-06 15:50:43 +0200

    added bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,12 +3,12 @@ class BankAccount:
         self.Account_balance = Account_balance
     def deposit(self, amount):
         self.Account_balance += amount
-        print(f"Deposit: {amount}")
+        print(f"Deposit: ${amount}")
     def withdraw(self, amount):
         self.Account_balance-= amount
         if amount <= self.Account_balance:
-            print(f"Withdraw: {amount}")
+            print(f"Withdraw: ${amount}")
         else:
             print("Insufficient funds.")
     def display_balance(self):
-        print(f"Current Balance: {self.Account_balance}")
+        print(f"Current Balance: ${self.Account_balance}")
