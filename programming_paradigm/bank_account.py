56624fdf8202d812aa9b6c75c84466c64958e336
Balungile <danca0717342125@gmail.com>
2024-10-06 15:13:38 +0200

    added bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,18 +1,20 @@
 class BankAccount:
-    def __init__(self, Account_balance = 0):
+    def __init__(self, Account_balance):
         self.Account_balance = Account_balance
-        
-    def deposit(self, Amount):
-        Amount = float(input("Amount you want to deposit: "))
-        self.Account_balance += Amount
-        print(f"Deposit: {Amount}")
-    def withdraw(self, Amount):
-        self.Amount = float(input("Amount you want to withdraw: "))
-        self.Account_balance -= Amount
-
-        if self.Account_balance >= Amount:
-            print(f"withdraw: {Amount}")
+    def deposit(self):
+        amount = float(input("Enter the amount you want to Deposit: "))
+        amount += self.Account_balance
+        print(f"Deposit: {amount}")
+    def withdraw(self):
+        amount = float(input("Enter the amount you want to withdraw: "))
+        amount -= self.Account_balance
+        if amount <= self.Account_balance:
+            print(f"Withdraw: {amount}")
         else:
             print("Insufficient funds.")
     def display_balance(self):
-        print(f"Current balance: {self.Account_balance}")
+        print(f"Current Balance: {self.Account_balance}")
+
+obj = BankAccount(20)
+obj.deposit()
+obj.withdraw()
