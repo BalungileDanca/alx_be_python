7b58ae186e738da7fb9f5c98695feeb0c9b5388a
Balungile <danca0717342125@gmail.com>
2024-10-06 17:13:54 +0200

    added bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,13 +1,13 @@
 class BankAccount:
     def __init__(self, Account_balance = 0):
-        self.Account_balance = Account_balance
+        self.__Account_balance = Account_balance
     def deposit(self, amount):
-           self.Account_balance += amount
+           self.__Account_balance += amount
     def withdraw(self, amount):
-           if amount > 0 and self.Account_balance >= amount:
-                self.Account_balance -= amount
+           if amount > 0 and self.__Account_balance >= amount:
+                self.__Account_balance -= amount
                 return True
-           elif amount > self.Account_balance:
+           elif amount > self.__Account_balance:
                 return False              
     def display_balance(self):
-        print(f"Current Balance: ${self.Account_balance}")
+        print(f"Current Balance: ${self.__Account_balance}")
