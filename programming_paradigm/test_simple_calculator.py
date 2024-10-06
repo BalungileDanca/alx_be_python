eb4eb5caf154182260d008db29202681f2972595
Balungile <danca0717342125@gmail.com>
2024-10-06 20:43:51 +0200

    test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,11 +7,11 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
     def test_addition(self):
         self.assertEqual(self.calc.add(8, 4), 12)
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(8, 4), 4)
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(8, 4), 32)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(8, 4), 2)
         assert self.calc.divide(10, 0) is None
 if __name__ == "__main__":
